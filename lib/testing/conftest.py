e8b1214579a27d57da2e00eca69f82c9cdcfbca2
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 14:36:40 +0300

    completed

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+# conftest.py
+
+import pytest
+from your_module import create_tables, insert_data  # Import your functions for creating tables and inserting data
+
+@pytest.fixture(autouse=True)
+def setup_database(conn):
+    create_tables(conn)
+    insert_data(conn)
