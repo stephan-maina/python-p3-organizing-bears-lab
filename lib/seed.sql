44719615cd28a25b1f8b725b3ba68f0ceb8c221b
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 14:31:25 +0300

    completed

--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,16 @@
-INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sergeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+-- seed.sql
+
+-- Create the Bears table
+CREATE TABLE IF NOT EXISTS bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER,
+    sex TEXT,
+    alive INTEGER
+);
+
+-- Insert sample data into the Bears table
+INSERT INTO bears (name, age, sex, alive) VALUES
+    ('Bear1', 5, 'M', 1),
+    ('Bear2', 3, 'F', 1),
+    ('Bear3', 8, 'M', 0);
