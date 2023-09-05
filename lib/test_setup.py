84b36e9a184015a6476f1eb465ad2a4a2eb4c72f
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 14:35:31 +0300

    completed

--- lib/test_setup.py
+++ lib/test_setup.py
@@ -1,2 +1,10 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# test_setup.py
+
+import pytest
+import sqlite3
+
+@pytest.fixture(scope="module")
+def conn():
+    conn = sqlite3.connect(":memory:")  # Create an in-memory SQLite database
+    yield conn
+    conn.close()
