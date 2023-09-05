8ea534b214ffd65f393297fba76d017ba7eea0f7
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 14:30:46 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+-- insert.sql
+
+-- Insert sample data into the Bears table
+INSERT INTO bears (name, age, sex, alive) VALUES
+    ('Bear1', 5, 'M', 1),
+    ('Bear2', 3, 'F', 1),
+    ('Bear3', 8, 'M', 0);
